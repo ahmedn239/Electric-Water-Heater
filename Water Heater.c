b40892cab13478a184ca1d12b00c683290e18072
ahmedn239 <60521354+ahmedn239@users.noreply.github.com>
2020-10-01 15:10:01 +0200

    Add files via upload

---
+++ Water Heater.c
@@ -0,0 +1,49 @@
+#include "Main.h"
+#include "Heat_Cool.h"
+#include "ADC.h"
+#include "SW.h"
+#include "Timer.h"
+#include "SSD.h"
+#include "Temp.h"
+#include "Display.h"
+#include "I2C.h"
+#include "Util.h"
+#include "LED.h"
+#include "Port.h"
+
+tWord __at(0x2007) CONFIG = _HS_OSC & _WDT_OFF & _PWRTE_ON & _BODEN_OFF & _LVP_OFF & _CPD_OFF & _WRT_OFF & _DEBUG_OFF & _CP_OFF;
+tByte flag = 0;
+
+
+void main (void)
+{
+    /* Initialization */
+    I2C_Master_Init();
+    SW_Init();
+    Heat_Init();
+    Cool_Init();
+
+    DISP_Init();
+    LED_Init(LED_1, LED_OFF);
+    TEMP_Init();
+
+    TMR_Init();
+    TMR_Start();
+
+    /* TRISC7 = 0;
+    RC7 = 0;*/
+
+    while (1)
+    {
+        /* Do nothing */
+        /* while (flag == 0)
+        {
+
+        }
+
+        flag = 0;*/
+
+    }
+
+}
+
